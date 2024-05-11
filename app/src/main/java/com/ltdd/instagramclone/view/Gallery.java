8b451f0cddbae6cdaff0d0594bbb3b28345b31a4
zerogit1 <lecongdat823@gmail.com>
2024-05-11 22:58:19 +0700

    add gallery fragment

---
+++ app/src/main/java/com/ltdd/instagramclone/view/Gallery.java
@@ -0,0 +1,260 @@
+package com.ltdd.instagramclone.view;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.ltdd.instagramclone.R;
+import com.ltdd.instagramclone.adapter.GridImageAdapter;
+import com.ltdd.instagramclone.model.FilePaths;
+import com.ltdd.instagramclone.model.FileSearch;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+
+import java.util.ArrayList;
+
+
+public class Gallery extends Fragment {
+    private static final String TAG = "GalleryFragment";
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+
+
+    //constants
+    private static final int NUM_GRID_COLUMNS = 4;
+    private static final String TAG1 = "PhotoFragment";
+    private static final int PHOTO_FRAGMENT_NUM = 1;
+    private static final int GALLERY_FRAGMENT_NUM = 2;
+    private static final int  CAMERA_REQUEST_CODE = 5;
+
+    //Test Firebase(nhớ delete)
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private Gallery mContext = Gallery.this;
+
+
+
+    //widgets
+    private GridView gridView;
+    private ImageView galleryImage;
+    private ProgressBar mProgressBar;
+    private Spinner directorySpinner;
+
+    //vars
+    private ArrayList<String> directories;
+    private String mAppend = "file:/";
+    private String mSelectedImage;
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.fragment_gallery, container, false);
+        galleryImage = (ImageView) view.findViewById(R.id.galleryImageView);
+        gridView = (GridView) view.findViewById(R.id.gridView);
+        directorySpinner = (Spinner) view.findViewById(R.id.spinnerDirectory);
+        mProgressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+        mProgressBar.setVisibility(View.GONE);
+        directories = new ArrayList<>();
+        Log.d(TAG, "onCreateView: started.");
+        myRef.setValue("Hello, World!");
+        ImageLoader.getInstance().init(ImageLoaderConfiguration.createDefault(getActivity()));
+        ImageView shareClose = (ImageView) view.findViewById(R.id.ivCloseShare);
+        shareClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: closing the gallery fragment.");
+                getActivity().finish();
+            }
+        });
+        ImageView btnLaunchCamera = (ImageView) view.findViewById(R.id.btnCamera);
+        btnLaunchCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: launching camera.");
+                Log.d(TAG, "onClick: starting camera");
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
+
+//                if(((StartActivity)getActivity()).getCurrentTabNumber() == PHOTO_FRAGMENT_NUM){
+//                    if(((StartActivity)getActivity()).checkPermissions(Permissions.CAMERA_PERMISSION[0])){
+//                        Log.d(TAG, "onClick: starting camera");
+//                        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                        startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
+//                    }else{
+//                        Intent intent = new Intent(getActivity(), StartActivity.class);
+//                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//                        startActivity(intent);
+//                    }
+//                }
+            }
+        });
+
+
+        TextView nextScreen = (TextView) view.findViewById(R.id.tvNext);
+        nextScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: navigating to the final share screen.");
+
+
+//                Intent intent = new Intent(getActivity(), NextActivity.class);
+//                intent.putExtra(getString(R.string.selected_image), mSelectedImage);
+//                startActivity(intent);
+
+//                }else{
+//                    Intent intent = new Intent(getActivity(), AccountSettingsActivity.class);
+//                    intent.putExtra(getString(R.string.selected_image), mSelectedImage);
+//                    intent.putExtra(getString(R.string.return_to_fragment), getString(R.string.edit_profile_fragment));
+//                    startActivity(intent);
+//                    getActivity().finish();
+//                }
+
+            }
+        });
+
+        init();
+
+
+        return view;
+    }
+    private void init(){
+        FilePaths filePaths = new FilePaths();
+
+        //check for other folders indide "/storage/emulated/0/pictures"
+        if (FileSearch.getDirectoryPaths(filePaths.DOWNLOADS) != null) {
+            directories = FileSearch.getDirectoryPaths(filePaths.DOWNLOADS);
+        }
+        directories.add(filePaths.DOWNLOADS);
+        directories.add(filePaths.PICTURES);
+
+        ArrayList<String> directoryNames = new ArrayList<>();
+        for (int i = 0; i < directories.size(); i++) {
+            Log.d(TAG, "init: directory: " + directories.get(i));
+            int index = directories.get(i).lastIndexOf("/");
+            String string = directories.get(i).substring(index).replace("/", "");
+            directoryNames.add(string);
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_spinner_item, directoryNames);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        directorySpinner.setAdapter(adapter);
+
+        directorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "onItemClick: selected: " + directories.get(position));
+
+                //setup our image grid for the directory chosen
+                setupGridView(directories.get(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+
+    private void setupGridView(String selectedDirectory){
+        Log.d(TAG, "setupGridView: directory chosen: " + selectedDirectory);
+        final ArrayList<String> imgURLs = FileSearch.getFilePaths(selectedDirectory);
+        if (imgURLs == null) {
+            Log.e(TAG, "setupGridView: imgURLs is null.");
+        } else {
+            Log.d(TAG, "setupGridView: Number of image URLs: " + imgURLs.size());
+        }
+
+        //set the grid column width
+        int gridWidth = getResources().getDisplayMetrics().widthPixels;
+        int imageWidth = gridWidth/NUM_GRID_COLUMNS;
+        gridView.setColumnWidth(imageWidth);
+
+        //use the grid adapter to adapter the images to gridview
+        GridImageAdapter adapter = new GridImageAdapter(getActivity(), R.layout.layout_grid_imageview, mAppend, imgURLs);
+        gridView.setAdapter(adapter);
+
+        //set the first image to be displayed when the activity fragment view is inflated
+        try{
+            if(imgURLs.size()!= 0){
+                setImage(imgURLs.get(0), galleryImage, mAppend);
+                mSelectedImage = imgURLs.get(0);
+
+            }
+
+        }catch (ArrayIndexOutOfBoundsException e){
+            Log.e(TAG, "setupGridView: ArrayIndexOutOfBoundsException: " +e.getMessage() );
+        }
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "onItemClick: selected an image: " + imgURLs.get(position));
+
+                setImage(imgURLs.get(position), galleryImage, mAppend);
+                mSelectedImage = imgURLs.get(position);
+            }
+        });
+
+    }
+
+
+    private void setImage(String imgURL, ImageView image, String append){
+        Log.d(TAG, "setImage: setting image");
+
+        ImageLoader imageLoader = ImageLoader.getInstance();
+
+        imageLoader.displayImage(append + imgURL, image, new ImageLoadingListener() {
+            @Override
+            public void onLoadingStarted(String imageUri, View view) {
+                mProgressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                mProgressBar.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                mProgressBar.setVisibility(View.INVISIBLE);
+            }
+
+            @Override
+            public void onLoadingCancelled(String imageUri, View view) {
+                mProgressBar.setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+
+}
