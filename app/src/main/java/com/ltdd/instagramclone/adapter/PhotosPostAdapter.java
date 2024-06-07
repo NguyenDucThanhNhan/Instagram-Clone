d2cc4347e97b3fd23e7cc54aaadb11b88cc81fa5
ndhuynh13 <ndhuynh13@gmail.com>
2024-06-07 23:21:37 +0700

    finish for ndhuynh

--- app/src/main/java/com/ltdd/instagramclone/adapter/PhotosPostAdapter.java
+++ app/src/main/java/com/ltdd/instagramclone/adapter/PhotosPostAdapter.java
@@ -1,17 +1,20 @@
 package com.ltdd.instagramclone.adapter;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.ltdd.instagramclone.R;
 import com.ltdd.instagramclone.model.Photo;
+import com.ltdd.instagramclone.view.DetailPostFragment;
 
 import java.util.List;
 
@@ -35,6 +38,25 @@ public class PhotosPostAdapter extends RecyclerView.Adapter<PhotosPostAdapter.Vi
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
         Photo photo = mPhotos.get(i);
         Glide.with(context).load(photo.getImage_path()).into(viewHolder.post_image);
+
+        viewHolder.post_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DetailPostFragment detailPostFragment = new DetailPostFragment();
+                // Truyền dữ liệu vào fragment thông qua Bundle
+                Bundle bundle = new Bundle();
+                bundle.putString("postid", photo.getPhoto_id());
+                bundle.putString("publisherid", photo.getUser_id());
+                detailPostFragment.setArguments(bundle);
+
+                // Thay đổi fragment bằng DetailPostFragment
+                FragmentActivity activity = (FragmentActivity) context;
+                activity.getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, detailPostFragment)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
     }
 
     @Override
